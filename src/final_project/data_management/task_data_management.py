ce7fca3cc5bc62694fc9f6813baf4dd70ec7d8ff
PurtiS <purtisadhwani92@gmail.com>
2023-03-29 21:09:32 +0200

    added data_management_tasks.

--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -5,20 +5,25 @@ import pytask
 
 from final_project.config import BLD, SRC
 from final_project.data_management import clean_data
-from final_project.utilities import read_yaml
 
 
 @pytask.mark.depends_on(
     {
-        "scripts": ["clean_data.py"],
-        "data_info": SRC / "data_management" / "data_info.yaml",
-        "data": SRC / "data" / "data.csv",
+        "data1": SRC / "data" / "pgen.dta",
+        "data2": SRC / "data" / "pl.dta",
+        "data3": SRC / "data" / "ppath.dta",
+        "data4": SRC / "data" / "hgen.dta",
+        "data5": SRC / "data" / "hbrutto.dta",
     },
 )
 @pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
 def task_clean_data_python(depends_on, produces):
     """Clean the data (Python version)."""
-    data_info = read_yaml(depends_on["data_info"])
-    data = pd.read_csv(depends_on["data"])
-    data = clean_data(data, data_info)
+    df1 = pd.read_stata(depends_on["data1"])
+    df5 = pd.read_stata(depends_on["data3"])
+    df6 = pd.read_stata(depends_on["data2"])
+    df_2013 = pd.read_stata(depends_on["data1"])
+    df7 = pd.read_stata(depends_on["data4"])
+    df8 = pd.read_stata(depends_on["data5"])
+    data = clean_data(df1=df1, df5=df5, df6=df6, df_2013=df_2013, df7=df7, df8=df8)
     data.to_csv(produces, index=False)
