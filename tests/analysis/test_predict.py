f8c68fc6c30361f0ea8ac7d6bfd6a7f1d7a44eae
PurtiS <purtisadhwani92@gmail.com>
2023-03-29 21:40:10 +0200

    added test data to test PS matching.

--- tests/analysis/test_predict.py
+++ tests/analysis/test_predict.py
@@ -2,45 +2,82 @@
 
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal
-from final_project.analysis.predict import predict_prob_by_age
+from psmpy.plotting import *
+
+import src.final_project.analysis.model as model_fn
+import src.final_project.analysis.predict as predict_fn
+from src.final_project.config import TEST_DIR
 
 
 @pytest.fixture()
 def data():
-    out = pd.DataFrame([1, 2, 3], columns=["age"])
-    out["education"] = ["high-school", "high-school", "university"]
-    out["income"] = ["high", "low", "low"]
-    return out
+    test_match_df = pd.read_csv(TEST_DIR / "analysis" / "test_match.csv")
+    return test_match_df
 
 
-@pytest.fixture()
-def model():
-    class ModelClass:
-        @staticmethod
-        def predict(data):
-            if "high-school" in data["education"].to_numpy():
-                prob = 0.1 if "low" in data["income"].to_numpy() else 0.2
-            else:
-                prob = 0.3 if "low" in data["income"].to_numpy() else 0.4
-            return prob * data["age"]
+def test_predicted_data(data):
+    expected_df = pd.DataFrame(
+        {
+            "marital_status": [1, 0, 0, 1, 0, 0, 1, 1, 1, 0],
+            "propensity_score": [
+                0.692085,
+                0.921919,
+                0.790986,
+                0.566517,
+                0.721278,
+                0.592211,
+                0.692085,
+                0.586988,
+                0.288803,
+                0.140653,
+            ],
+        },
+    )
+    psm = model_fn.create_psm(
+        data,
+        treatment="went_unemployed",
+        indx="pid",
+        exclude=["hid", "aggregate_loneliness_2017"],
+    )
+    model_fn.run_logistic_ps(psm, balance=False)
+    output = predict_fn.get_predicted_data(psm).reset_index(drop=True)
+    output = output[["marital_status", "propensity_score"]]
+    output["propensity_score"] = output["propensity_score"].round(6)
+    pd.testing.assert_frame_equal(expected_df, output)
 
-    return ModelClass
 
+def test_matched_df(data):
+    expected_df = pd.DataFrame(
+        {
+            "marital_status": [1, 1, 1, 0, 0, 1, 0],
+            "propensity_score": [
+                0.692085,
+                0.586988,
+                0.288803,
+                0.140653,
+                0.592211,
+                0.566517,
+                0.721278,
+            ],
+            "matched_ID": [2.0, 9.0, 7.0, 8.0, np.nan, np.nan, np.nan],
+        },
+    )
 
-@pytest.mark.parametrize("group", ["education", "income"])
-def test_predict_prob_over_age(data, model, group):
-    got = predict_prob_by_age(data, model, group)
-
-    if group == "education":
-        expected = pd.DataFrame(
-            [[1, 0.1, 0.3], [2, 0.2, 0.6], [3, 0.3, 0.9]],
-            columns=["age", "high-school", "university"],
-        )
-    else:
-        expected = pd.DataFrame(
-            [[1, 0.2, 0.1], [2, 0.4, 0.2], [3, 0.6, 0.3]],
-            columns=["age", "high", "low"],
-        )
-
-    assert_frame_equal(got, expected)
+    psm = model_fn.create_psm(
+        data,
+        treatment="went_unemployed",
+        indx="pid",
+        exclude=["hid", "aggregate_loneliness_2017"],
+    )
+    model_fn.run_logistic_ps(psm, balance=False)
+    predict_fn.get_predicted_data(psm).reset_index(drop=True)
+    predict_fn.run_knn_matched(
+        psm,
+        matcher="propensity_score",
+        replacement=False,
+        caliper=None,
+    )
+    output = predict_fn.matched_df(psm, data).reset_index(drop=True)
+    output = output[["marital_status", "propensity_score", "matched_ID"]]
+    output["propensity_score"] = output["propensity_score"].round(6)
+    pd.testing.assert_frame_equal(expected_df, output)
